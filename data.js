5948f1f01d819b89b166d713618ae1feb7f2e155
vnnisingh <vnnisingh@gmail.com>
2026-05-02 18:23:24 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    stream: "BSC",
+    year: "Second Year",
+    category: "Foundation",
+    subject: "English",
+    date: "10 May",
+    time: "9 AM"
+  },
+  {
+    stream: "BA",
+    year: "First Year",
+    category: "Major",
+    subject: "History",
+    date: "12 May",
+    time: "2 PM"
+  }
+];
