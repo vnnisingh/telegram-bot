ba7cec917ac16921422f25d569e7e55e5bae5c25
vnnisingh <vnnisingh@gmail.com>
2026-05-02 18:24:24 +0530

    Create buttons.js

---
+++ buttons.js
@@ -0,0 +1,12 @@
+exports.streamButtons = [
+  ["BA"], ["BSC"], ["BCOM"], ["MSC"], ["MA"], ["MCOM"]
+];
+
+exports.yearButtons = [
+  ["First Year"], ["Second Year"], ["Third Year"], ["Final Year"]
+];
+
+exports.categoryButtons = [
+  ["Foundation"], ["Major"], ["Major 2"],
+  ["Minor"], ["Open Elective"], ["Vocational"]
+];
