3bfe0c04d04e3ab64dd935916146298e1bbc4b8e
vnnisingh <vnnisingh@gmail.com>
2026-05-03 13:47:07 +0530

    Added start command handler

--- handlers/start.js
+++ handlers/start.js
@@ -1,1 +1,15 @@
+module.exports = (bot) => {
+  bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id,
+`🤖 Welcome to your bot!
 
+Choose an option 👇`, {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "📂 Menu", callback_data: "menu" }],
+          [{ text: "ℹ️ About", callback_data: "about" }]
+        ]
+      }
+    });
+  });
+};
